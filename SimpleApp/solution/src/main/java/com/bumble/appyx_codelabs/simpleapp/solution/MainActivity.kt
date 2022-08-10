34709309cc2d1ea13af84ba058d048fcc6435c16
samuelps <samuel.perezsarmiento@team.bumble.com>
2022-08-10 16:52:07 +0200

    update solution to match app initial initial status

--- SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simpleapp/solution/MainActivity.kt
+++ SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simpleapp/solution/MainActivity.kt
@@ -2,13 +2,9 @@ package com.bumble.appyx_codelabs.simpleapp.solution
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import com.bumble.appyx.core.integration.NodeHost
 import com.bumble.appyx.core.integrationpoint.NodeActivity
-import com.bumble.appyx_codelabs.simpleapp.solution.node.root.RootNode
+import com.bumble.appyx_codelabs.simpleapp.solution.root.RootNode
 import com.bumble.appyx_codelabs.simpleapp.solution.ui.theme.SimpleAppTheme
 
 class MainActivity : NodeActivity() {
@@ -17,13 +13,8 @@ class MainActivity : NodeActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    NodeHost(integrationPoint = integrationPoint) {
-                        RootNode(it)
-                    }
+                NodeHost(integrationPoint = integrationPoint) {
+                    RootNode(buildContext = it)
                 }
             }
         }
