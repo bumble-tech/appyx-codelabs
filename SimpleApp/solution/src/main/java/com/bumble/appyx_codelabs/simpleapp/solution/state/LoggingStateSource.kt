b68e03ec62170503136b57731fb588d4ced299c3
samuelps <samuel.perezsarmiento@team.bumble.com>
2022-08-11 17:18:00 +0200

    state based navigation

---
+++ SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simpleapp/solution/state/LoggingStateSource.kt
@@ -0,0 +1,6 @@
+package com.bumble.appyx_codelabs.simpleapp.solution.state
+
+interface LogginState {
+
+    
+}
---
+++ SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simpleapp/solution/state/LoggingStateSourceImpl.kt
@@ -0,0 +1,4 @@
+package com.bumble.appyx_codelabs.simpleapp.solution.state
+
+class LoggingStateSourceImpl {
+}
