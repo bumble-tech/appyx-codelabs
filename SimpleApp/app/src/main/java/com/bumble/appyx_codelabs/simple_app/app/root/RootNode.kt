2383779e6f03207ec072dbc7f10412ef63a810e7
samuelps <samuel.perezsarmiento@team.bumble.com>
2022-08-26 15:45:52 +0200

    set simple app repo code in the initial status

---
+++ SimpleApp/app/src/main/java/com/bumble/appyx_codelabs/simple_app/app/root/RootNode.kt
@@ -0,0 +1,33 @@
+package com.bumble.appyx_codelabs.simple_app.app.root
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bumble.appyx.core.modality.BuildContext
+import com.bumble.appyx.core.node.Node
+import com.bumble.appyx_codelabs.simple_app.app.ui.theme.SimpleAppTheme
+
+class RootNode(
+    buildContext: BuildContext,
+    private val name: String,
+) : Node(buildContext) {
+
+    @Composable
+    override fun View(modifier: Modifier) {
+        Greeting(name = name)
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Welcome to Appyx, $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SimpleAppTheme {
+        Greeting("Android")
+    }
+}
--- SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simple_app/solution/MainActivity.kt
+++ SimpleApp/solution/src/main/java/com/bumble/appyx_codelabs/simple_app/solution/MainActivity.kt
@@ -1,43 +1,21 @@
 package com.bumble.appyx_codelabs.simple_app.solution
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.bumble.appyx.core.integration.NodeHost
+import com.bumble.appyx.core.integrationpoint.NodeActivity
+import com.bumble.appyx_codelabs.simple_app.solution.root.RootNode
 import com.bumble.appyx_codelabs.simple_app.solution.ui.theme.SimpleAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : NodeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                NodeHost(integrationPoint = integrationPoint) {
+                    RootNode(it, "Jon Doe")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimpleAppTheme {
-        Greeting("Android")
     }
 }
