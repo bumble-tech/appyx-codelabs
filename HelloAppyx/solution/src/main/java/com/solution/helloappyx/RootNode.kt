2e7f6937f4e0009de4dd27eddd0a04ec27fc7a50
vladcipariu <vladcipariu@gmail.com>
2022-10-04 12:02:41 +0100

    Add HelloAppyx codelab

---
+++ HelloAppyx/solution/src/main/java/com/solution/helloappyx/RootNode.kt
@@ -0,0 +1,59 @@
+package com.solution.helloappyx
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumble.appyx.core.integration.NodeHost
+import com.bumble.appyx.core.integrationpoint.IntegrationPointStub
+import com.bumble.appyx.core.modality.BuildContext
+import com.bumble.appyx.core.node.Node
+
+class RootNode(buildContext: BuildContext) : Node(buildContext = buildContext) {
+
+    @Composable
+    override fun View(modifier: Modifier) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.background),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Appyx Logo",
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "Hello Appyx!",
+                fontSize = 36.sp,
+                color = MaterialTheme.colors.onBackground
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+@ExperimentalUnitApi
+@ExperimentalComposeUiApi
+fun SimpleGraphNodePreview() {
+    Box(Modifier.fillMaxSize()) {
+        NodeHost(integrationPoint = IntegrationPointStub()) {
+            RootNode(
+                buildContext = BuildContext.root(null)
+            )
+        }
+    }
+}
