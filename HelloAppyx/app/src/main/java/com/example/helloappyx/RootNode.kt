0198c0e4c4adef58085e8783326b44cea08a27b2
vladcipariu <vladcipariu@gmail.com>
2022-10-26 13:04:15 +0100

    remove projects

--- HelloAppyx/app/src/main/java/com/example/helloappyx/RootNode.kt
+++
@@ -1,5 +0,0 @@
-package com.example.helloappyx
-
-// TODO: (2) Inherit from Node, add the required parameters to the constructor and override the required function
-class RootNode {
-}
--- SimpleApp/app/src/main/java/com/bumble/appyx_codelabs/simple_app/app/child/Child1Node.kt
+++
@@ -1,5 +0,0 @@
-package com.bumble.appyx_codelabs.simple_app.app.child
-
-// TODO: (6.1) Create the first child node with a button
-class Child1Node {
-}
